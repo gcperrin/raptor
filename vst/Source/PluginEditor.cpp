8c4348074c37c49e83d2469c669ade4ff7f3ef35
Greg Perrin <gregoryperrin.ucb@gmail.com>
2020-03-16 22:14:12 -0600

    [BUILD] Initial commit, libs.

---
+++ vst/Source/PluginEditor.cpp
@@ -0,0 +1,62 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+
+//==============================================================================
+VstAudioProcessorEditor::VstAudioProcessorEditor (VstAudioProcessor& p)
+    : AudioProcessorEditor (&p), processor (p)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+
+    setSize (800, 600);
+
+}
+
+VstAudioProcessorEditor::~VstAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void VstAudioProcessorEditor::paint (Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+
+    g.setColour (Colours::white);
+    g.setFont (15.0f);
+    g.drawFittedText ("Hello puppii!", getLocalBounds(), Justification::centred, 1);
+
+}
+
+void VstAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+  slider.reset (new Slider ("new slider"));
+    addAndMakeVisible (slider.get());
+    slider->setRange (0, 10, 0);
+    slider->setSliderStyle (Slider::LinearHorizontal);
+    slider->setTextBoxStyle (Slider::TextBoxLeft, false, 80, 20);
+    // slider->addListener (this);
+
+    slider->setBounds (200, 448, 150, 24);
+
+    toggleButton.reset (new ToggleButton ("yung_money"));
+    addAndMakeVisible (toggleButton.get());
+    // toggleButton->addListener (this);
+
+    toggleButton->setBounds (488, 448, 150, 24);
+
+
+}
